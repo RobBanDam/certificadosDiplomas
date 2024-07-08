385d7625feda9ced26b4a7eda58cf73ead93081a
RobBanDam <robert.mtz.a.08@gmail.com>
2024-07-07 22:33:31 -0600

    Creando Maquetacion HMTL v5

--- views/html/MainMenu.php
+++ views/html/MainMenu.php
@@ -1,24 +1,30 @@
 <!-- ########## START: LEFT PANEL ########## -->
-<div class="br-logo"><a href=""><span>[</span>Empresa<span>]</span></a></div>
+<div class="br-logo"><a href="../../views/UsuHome/"><span>[</span>Empresa<span>]</span></a></div>
 <div class="br-sideleft overflow-y-auto">
 	<label class="sidebar-label pd-x-15 mg-t-20">Menú</label>
 	<div class="br-sideleft-menu">
-		<a href="index.html" class="br-menu-link">
+		<a href="../../views/UsuHome/" class="br-menu-link">
 			<div class="br-menu-item">
 				<i class="menu-item-icon icon ion-ios-home-outline tx-22"></i>
 				<span class="menu-item-label">Inicio</span>
 			</div><!-- menu-item -->
 		</a><!-- br-menu-link -->
-		<a href="mailbox.html" class="br-menu-link">
+		<a href="../../views/UsuCurso/" class="br-menu-link">
 			<div class="br-menu-item">
 				<i class="menu-item-icon icon ion-ios-bookmarks-outline tx-20"></i>
 				<span class="menu-item-label">Mis Cursos</span>
 			</div><!-- menu-item -->
 		</a><!-- br-menu-link -->
-		<a href="card-dashboard.html" class="br-menu-link">
+		<a href="../../views/UsuPerfil/" class="br-menu-link">
 			<div class="br-menu-item">
 				<i class="menu-item-icon icon ion-ios-gear-outline tx-24"></i>
 				<span class="menu-item-label">Perfil</span>
+			</div><!-- menu-item -->
+		</a><!-- br-menu-link -->
+		<a href="#" class="br-menu-link">
+			<div class="br-menu-item">
+				<i class="menu-item-icon icon ion-power tx-20"></i>
+				<span class="menu-item-label">Cerrar Sesión</span>
 			</div><!-- menu-item -->
 		</a><!-- br-menu-link -->
 	</div><!-- br-sideleft-menu -->
